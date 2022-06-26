0f88b6d4401824da0c603b55bb1ff11125a35dc5
SieR_VR <nwh63@naver.com>
2022-06-26 15:34:39 +0900

    process env?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+  }
 }
 
 module.exports = nextConfig
