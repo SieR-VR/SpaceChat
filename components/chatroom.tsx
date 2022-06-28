d28f4a5f4fa59abd83ebf6d2da3f7d0e7def816d
SieR_VR <nwh63@naver.com>
2022-06-29 00:12:20 +0900

    Very Hotfix

--- components/chatroom.tsx
+++ components/chatroom.tsx
@@ -12,12 +12,17 @@ import {
 import Message from './message';
 import ChannelHeader from './channelHeader';
 import Input from './input';
+import { useSession } from 'next-auth/react';
 
 export default function Chatroom({ client, spaceId }: { client: StreamChat, spaceId: string }) {
   const [channel, setChannel] = useState<StreamChatChannel | null>(null);
+  const { data: session } = useSession();
+
   useEffect(() => {
     (async () => {
-      const userInfo = await fetch("/api/twitter/user", { method: "GET", })
+      const userInfo = await fetch("/api/twitter/user", { method: "POST", body: JSON.stringify({
+        accessToken: session.accessToken,
+      })})
         .then(res => res.json());
       const streamToken = await fetch("/api/user", { method: "POST", body: JSON.stringify({ id: userInfo.user.id }) })
         .then(res => res.json());
